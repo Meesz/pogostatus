53968b3f67e9ab66407dbe1ef7a746b662164429
Meesz <20428509+Meesz@users.noreply.github.com>
2024-08-20 05:11:45 +0200

    commit

--- app/api/user/route.ts
+++ app/api/user/route.ts
@@ -1,38 +1,39 @@
-// // app/api/user/route.ts
+// app/api/user/route.ts
 
-// import { NextResponse } from "next/server";
-// import { PrismaClient } from "@prisma/client";
-// import bcrypt from "bcrypt";
-// const prisma = new PrismaClient();
+import { NextResponse } from "next/server";
+import { PrismaClient } from "@prisma/client";
+import bcrypt from "bcrypt";
+const prisma = new PrismaClient();
 
-// // POST request to create a user
-// export async function POST(request: Request) {
-//   try {
-//     const { password } = await request.json();
+// POST request to create a user
+export async function POST(request: Request) {
+  return;
+  try {
+    const { password } = await request.json();
 
-//     if (!password) {
-//       return NextResponse.json(
-//         { error: "Password is required" },
-//         { status: 400 }
-//       );
-//     }
+    if (!password) {
+      return NextResponse.json(
+        { error: "Password is required" },
+        { status: 400 }
+      );
+    }
 
-//     // Hash the password using bcrypt
-//     const saltRounds = 10;
-//     const hashedPassword = await bcrypt.hash(password, saltRounds);
+    // Hash the password using bcrypt
+    const saltRounds = 10;
+    const hashedPassword = await bcrypt.hash(password, saltRounds);
 
-//     const user = await prisma.user.create({
-//       data: { password: hashedPassword },
-//     });
+    const user = await prisma.user.create({
+      data: { password: hashedPassword },
+    });
 
-//     return NextResponse.json(user, { status: 201 });
-//   } catch (error) {
-//     console.error("Error creating user:", error);
-//     return NextResponse.json(
-//       { error: "Internal server error" },
-//       { status: 500 }
-//     );
-//   } finally {
-//     await prisma.$disconnect();
-//   }
-// }
+    return NextResponse.json(user, { status: 201 });
+  } catch (error) {
+    console.error("Error creating user:", error);
+    return NextResponse.json(
+      { error: "Internal server error" },
+      { status: 500 }
+    );
+  } finally {
+    await prisma.$disconnect();
+  }
+}
